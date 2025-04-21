10f09b132cb2f1f8b6612b268752cca0b37b6a7d
Catalina Pesquet <catalina.pesquet@gmail.com>
2025-04-21 11:37:12 -0600

    Add files via upload

---
+++ metrics.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 20 18:38:33 2025
+
+@author: Catalina
+"""
+
+from pymoo.core.problem import ElementwiseProblem
+from encode_decode import decode  # à adapter selon ton code
+from DecisionTree import DecisionTree
+from sklearn.metrics import recall_score, f1_score, confusion_matrix
+import numpy as np
+
+# Complexity: counting nodes
+def count_nodes(node):
+    if node is None:
+        return 0
+    if node.is_leaf:
+        return 1
+    return 1 + count_nodes(node.left) + count_nodes(node.right)
+
+# Evaluates tree with chosen metric (f1, specificity, recall)
+def evaluate_tree(tree, X_test, y_test):
+    y_pred = tree.predict(X_test)
+
+    f1 = f1_score(y_test, y_pred, average='macro')
+    recall = recall_score(y_test, y_pred, average='macro')
+
+    cm = confusion_matrix(y_test, y_pred)
+    if cm.shape == (2, 2):
+        tn, fp, fn, tp = cm.ravel()
+        specificity = tn / (tn + fp) if (tn + fp) != 0 else 0.0
+    else:
+        specificity = 0.0
+
+    n_nodes = count_nodes(tree.tree_)
+
+    return {
+        "f1": f1,
+        "recall": recall,
+        "specificity": specificity,
+        "n_nodes": n_nodes
+    }
---
+++ pymoo_implementation.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 20 15:26:37 2025
+
+@author: Catalina
+"""
+
+from pymoo.core.problem import ElementwiseProblem
+from encode_decode import decode
+from metrics import evaluate_tree 
+import numpy as np
+
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.operators.crossover.sbx import SBX
+from pymoo.operators.mutation.pm import PM
+from pymoo.operators.sampling.rnd import FloatRandomSampling
+from pymoo.termination import get_termination
+from pymoo.optimize import minimize
+
+class TreeProblem(ElementwiseProblem):
+    def __init__(self, X_train, y_train, X_test, y_test, objectives):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
+        self.selected_objectives = objectives
+        super().__init__(
+            n_var=8,
+            n_obj=2,
+            xl=np.array([0, 0, 0, 0, 0, 0, 0, 0]),
+            xu=np.array([12, 4, 100, 3, 6, 1, 5, 100]),
+            type_var=np.int32
+            )
+    
+    def _evaluate(self, x, out, *args, **kwargs):
+        try:
+            # Decode tree
+            tree = decode(x)
+            
+            # Training + evaluation
+            metrics = evaluate_tree(tree, self.X_test, self.y_test)
+            
+            # Extract chosen objectives
+            values = []
+            for obj in self.selected_objectives:
+                val = metrics[obj]
+                # Check if the metrics is to be maximized 
+                if obj in ['f1', 'recall', 'specificity']:
+                    values.append(1 - val)
+                else: 
+                    values.append(val)
+            
+            # Objectives to minimize
+            out["F"] = values
+            
+        except Exception as e:
+            # In case of fail in the construction of the tree
+            out["F"] = [1.0, 999]
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+X, y = iris.data, iris.target
+X_meta_train, X_meta_test, y_meta_train, y_meta_test = train_test_split(X, y, train_size=0.9)
+X_train, X_test, y_train, y_test = train_test_split(X_meta_train, y_meta_train)
+problem = TreeProblem(
+    X_train=X_train,
+    y_train=y_train,
+    X_test=X_test,
+    y_test=y_test,
+    objectives=["recall", "specificity"]  
+)
+algorithm = NSGA2(pop_size=100)
+termination = get_termination("n_gen", 50)
+
+res = minimize(problem,
+               algorithm,
+               termination,
+               seed=42,
+               verbose=True)
+        
